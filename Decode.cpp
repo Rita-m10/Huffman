e2bc0f9ac0caddb3d72015a44e5f220bdf0ff984
Rita-m10 <82998721+Rita-m10@users.noreply.github.com>
2022-01-31 12:34:18 +0700

    Create Decode.cpp

---
+++ Decode.cpp
@@ -0,0 +1,23 @@
+#include "Header.h"
+
+int main()
+{
+    ifstream In("Encoded.txt", ios::binary);
+    if (!In.is_open())
+    {
+        cout << "error";
+        return 0;
+    }
+    ofstream Out("Decoded.txt", ios::binary);
+    if (!In.is_open())
+    {
+        cout << "error";
+    }
+    Arithmetic a(10);
+    a.ReadCap(In);
+   // a.PrintMap();
+    a.Decoded(In, Out);
+    In.close();
+    Out.close();
+    return 0;
+}
