a8cfe41cc1752eb581f85693b7f70ffeec246e02
Rita-m10 <82998721+Rita-m10@users.noreply.github.com>
2022-03-12 15:31:47 +0700

    Update HuffUnzip.cpp

--- HuffUnzip.cpp
+++ HuffUnzip.cpp
@@ -34,3 +34,19 @@ class Node{
         key = l->key + r->key;
     }
 };
+
+void TreeGo(Node* Head, vector<bool>code, map<char, vector<bool>> *buff){
+    if (Head->left){
+	    code.push_back(0);
+        TreeGo(Head->left, code, buff);
+    }
+    if (Head->right){
+        code.push_back(1);
+        TreeGo(Head->right, code, buff);
+	}
+	if (((Head->right) == NULL) && ((Head->left) == NULL))
+        (*buff)[Head->s] = code;
+    if (!code.empty()) 
+		code.pop_back();
+}
+
