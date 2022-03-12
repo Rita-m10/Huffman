67cb58b5d18cf1238eacadfa18bc3d13e18f6cdc
Rita-m10 <82998721+Rita-m10@users.noreply.github.com>
2022-03-12 20:57:02 +0700

    Update HuffUnzip.cpp

--- HuffUnzip.cpp
+++ HuffUnzip.cpp
@@ -9,8 +9,8 @@ using namespace std;
 
 class Node{
     public:
-    int key;
-    unsigned char s;
+    int key;            //Частота
+    unsigned char s;    //Символ
     Node *left;
     Node *right;
     
@@ -18,15 +18,15 @@ class Node{
         key = 0;
         s = 0;
         left = nullptr;
-		right = nullptr;
+	right = nullptr;
     }
 	
-	Node(unsigned char _s, int k){
-	    s = _s;
-		key = k;
-		left = nullptr;
-		right = nullptr;
-	}
+    Node(unsigned char _s, int k){
+	s = _s;
+	key = k;
+	left = nullptr;
+	right = nullptr;
+    }
 
     Node(Node *l, Node *r){
         left =  l;
@@ -35,18 +35,48 @@ class Node{
     }
 };
 
+//Леворекурсивный обход дерева 
 void TreeGo(Node* Head, vector<bool>code, map<char, vector<bool>> *buff){
     if (Head->left){
-	    code.push_back(0);
-        TreeGo(Head->left, code, buff);
+	vector <bool> temp = code;
+	temp.push_back(0);
+        TreeGo(Head->left, temp, buff);
     }
     if (Head->right){
-        code.push_back(1);
-        TreeGo(Head->right, code, buff);
-	}
-	if (((Head->right) == NULL) && ((Head->left) == NULL))
+	vector <bool> temp = code;
+        temp.push_back(1);
+        TreeGo(Head->right, temp, buff);
+    }
+    if (Head->right) == nullptr && Head->left == nullptr)
         (*buff)[Head->s] = code;
     if (!code.empty()) 
-		code.pop_back();
+	code.pop_back();
 }
+
+int main()
+{
+    //Сжатый файл
+    ifstream In("B.txt", ios::binary);
+    if (!In.is_open()){ 
+        cout << "error"; 
+        return 0; 
+    }
+	
+    //Файл, куда записываем результат расжатия 
+    ofstream Out("C.txt");
+    if (!Out.is_open()){ 
+        cout << "error"; 
+        return 0; 
+    }
+	
+    //Количество значимых бит в последнем байте
+    int lastByte;
+    In.read((char*)&lastByte, sizeof(lastByte));
+
+    //Количество пар (символ - частота)
+    int numberOfPairs;
+    In.read((char*)&numberOfPairs, sizeof(numberOfPairs));
+	
+    
+    
 
