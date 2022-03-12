17d34850ff134870d298eb3c0dee4f52ae41434f
Rita-m10 <82998721+Rita-m10@users.noreply.github.com>
2022-03-12 20:46:46 +0700

    Update HuffZip.cpp

--- HuffZip.cpp
+++ HuffZip.cpp
@@ -7,11 +7,12 @@
 
 using namespace std;
 
+//Клас узел
 class Node
 {
     public:
-    int key;
-    unsigned char s;
+    int key;           //Частота
+    unsigned char s;   //Символ
     Node *left;
     Node *right;
     
@@ -36,61 +37,82 @@ class Node
     }
 };
 
+//Леворекурсивный обход дерева и присваивание булева вектора каждому символу
 void TreeGo(Node* Head, vector<bool>code, map<char, vector<bool>> *buff){
     if (Head->left){
-	code.push_back(0);
-        TreeGo(Head->left, code, buff);
+	vector <bool> temp = code;
+	temp.push_back(0);
+        TreeGo(Head->left, temp, buff);
     }
     if (Head->right){
-        code.push_back(1);
-        TreeGo(Head->right, code, buff);
+	vector <bool> temp = code;
+        temp.push_back(1);
+        TreeGo(Head->right, temp, buff);
     }
-    if (((Head->right) == NULL) && ((Head->left) == NULL))
+    if (Head->right) == nullptr && Head->left == nullptr)
         (*buff)[Head->s] = code;
     if (!code.empty()) 
         code.pop_back();
 }
 
 int main(){
-	ifstream In("A.txt");
-	ofstream Out("B.txt", ios::binary);
-	int Q[256]= {0};
-	unsigned char s = 0;
+    //Оригинальный текст
+    ifstream In("A.txt");
+    if (!In.is_open()){ 
+        cout << "error"; 
+	return 0; 
+    }
+    //Сжатый файл
+    ofstream Out("B.txt", ios::binary);
+    if (!Out.is_open()){ 
+        cout << "error"; 
+        return 0; 
+    }
 	
-	while((s = In.get()) && !In.eof())
-		Q[s]++;
+    //Подсчет частоты появления каждого символа в тексте
+    int Q[256]= {0};
+    unsigned char s = 0;
+
+    while((s = In.get()) && !In.eof())
+	Q[s]++;
 	
-	In.clear();
+    //Указатель на начало файла
+    In.clear();
     In.seekg(0, ios_base::beg);
-	
-	list<Node*> List;
-	for (int i=0; i<256; i++){
-		if (Q[i]==0)
-			continue;
-		Node* uzel = new Node(i, Q[i]);
-		auto iter = List.begin();
-		for (; iter != List.end();){
-			if (uzel->key > (*iter)->key)
-				iter++;
-			else 
-				break;
-		}
-		List.insert(iter, uzel);
+
+    //Список узлов
+    list<Node*> List;
+    for (int i=0; i<256; i++){
+        if (Q[i]==0)
+	    continue;
+	Node* uzel = new Node(i, Q[i]);
+	auto iter = List.begin();
+	for (; iter != List.end();){
+	    if (uzel->key > (*iter)->key)
+                iter++;
+	    else 
+	        break;
 	}
+	List.insert(iter, uzel);
+    }
 	
-	int n = 0;
-	Out.write((char*)&n, sizeof(n));
-	
-	n = List.size();
-	Out.write((char*)&n, sizeof(n));
-	
-	for (auto it: List){
-		Out.write((char*)&it->s, sizeof(it->s));
+    //Занимаем место в начале файла
+    int n = 0;
+    Out.write((char*)&n, sizeof(n));
+
+    //Записываем количество пар (символ - частота)
+    n = List.size();
+    Out.write((char*)&n, sizeof(n));
+
+    //Записываем пары (символ - частота)
+    for (auto it: List){
+        Out.write((char*)&it->s, sizeof(it->s));
         Out.write((char*)&it->key, sizeof(it->key));
     }
 	
-	while (List.size() != 1){
-		Node *l = List.front();
+    //Строим дерево по списку
+    while (List.size() != 1){
+        Node *l = List.front();
         List.pop_front();
         Node *r = List.front();
         List.pop_front();
@@ -99,25 +121,21 @@ int main(){
 
         auto iter = List.begin();
 
-        for (; iter != List.end();)
-        {
+        for (; iter != List.end();){
             if (newUzel->key > (*iter)->key)
-            {
                 iter++;
-                continue;
-            }
             else
-            {
                 break;
-            }
         }
         List.insert(iter, newUzel);
     }
 	
-	vector<bool> code;
+    //Присваиваем каждому символу булев вектор
+    vector<bool> code;
     map<char, vector<bool>>* buff = new map<char, vector<bool>>;
     TreeGo(*(List.begin()), code, buff);
 
+    //Формируем новые байты и записываем их в файл
     unsigned char tx = 0;
     int count = 0;
     while ((s = In.get()) && !In.eof()){
@@ -133,12 +151,15 @@ int main(){
         }
     }
 
+    //Записываем последний байт
     if (!(count==0 && tx==0))
         Out.write((char*)&tx, 1);   
 
+    //Настраиваемся на начало файла и записываем количество значимых бит в последнем байте
     Out.clear();
     Out.seekp(0);
     Out.write((char*)&count, sizeof(count));
+
     Out.close();
     In.close();    
 
